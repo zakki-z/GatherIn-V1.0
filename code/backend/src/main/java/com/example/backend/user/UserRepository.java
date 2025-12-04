e2c0e5c0180ee25f999f0cc0a97a57b029a48f12
zaki azhari <zakariaeazhari@proton.me>
2025-12-04 16:25:59 +0100

    create UserService

--- code/backend/src/main/java/com/example/backend/user/UserRepository.java
+++ code/backend/src/main/java/com/example/backend/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.example.backend.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findAllByStatus(Status status);
 }
